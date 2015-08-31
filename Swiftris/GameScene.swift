4992e74dbf9cec8689d35fb98b4bd80e917240dd
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-08-30 22:32:47 -0700

    Set up Block.swift

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,11 +8,15 @@
 
 import SpriteKit
 
+//#1
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
     
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+    //#2
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick: NSDate?
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -27,5 +31,28 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        
+        //#3
+        if lastTick == nil {
+            return
+        }
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    //#4
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
