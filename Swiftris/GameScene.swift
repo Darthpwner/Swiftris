081672f5fe4ebbce656f01e4ee0f8b8a3b67d69c
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-08-31 23:50:46 -0700

    Continue tomorrow, too tired

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -9,14 +9,23 @@
 import SpriteKit
 
 //#1
+let BlockSize: CGFloat = 20.0
+
 let TickLengthLevelOne = NSTimeInterval(600)
 
 class GameScene: SKScene {
     
     //#2
+    let gameLayer = SKNode()
+    let shapeLayer = SKNode()
+    let LayerPosition = CGPoint(x: 6, y: -6)
+    
+    //#2
     var tick:(() -> ())?
     var tickLengthMillis = TickLengthLevelOne
     var lastTick: NSDate?
+    
+    var textureCache = Dictionary<String, SKTexture>()
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
@@ -31,6 +40,17 @@ class GameScene: SKScene {
         background.position = CGPoint(x: 0, y: 0)
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
+    
+        addChild(gameLayer)
+        
+        let gameBoardTexture = SKTexture(imageNamed: "gameboard")
+        let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSizeMake(BlockSize * CGFloat(NumColumns), BlockSize * CGFloat(NumRows)))
+        gameBoard.anchorPoint = CGPoint(x: 0, y: 1.0)
+        gameBoard.position = LayerPosition
+        
+        shapeLayer.position = LayerPosition
+        shapeLayer.addChild(gameBoard)
+        gameLayer.addChild(shapeLayer)
     }
     
     override func update(currentTime: CFTimeInterval) {
@@ -55,4 +75,6 @@ class GameScene: SKScene {
     func stopTicking() {
         lastTick = nil
     }
+    
+    //#3
 }
